f978e0bd5d3f0594987e8745065862e29c233ac4
Brian Neufeld <36906721+brain1098@users.noreply.github.com>
2022-09-13 00:05:18 -0400

    Update application.py

--- application.py
+++ application.py
@@ -45,10 +45,10 @@ encrypt_check = tk.IntVar()
 
 tabControl = ttk.Notebook(root)
   
-tab1 = ttk.Frame(tabControl)
+
 tab2 = ttk.Frame(tabControl)
 tab3 = ttk.Frame(tabControl)
-  
+tab1 = ttk.Frame(tabControl)
 tabControl.add(tab1, text ='Image encoding')
 tabControl.add(tab2, text ='Image decoding')
 tabControl.add(tab3, text ='Audio encrypting')
@@ -56,6 +56,324 @@ tabControl.pack(expand = 1, fill ="both")
 
 def get_digit(number, n):
     return number // 10**n % 10
+
+
+class tab1_class:
+
+    def __init__(self, master):
+
+        global base_img_filename
+        global audio_filename
+        global audioarray
+
+        global open_audio_file_btn, open_audio_file_btn, preview_btn, encode_btn
+
+        open_img_file_btn = tk.Button(tab1, text="Open image file", height = 2, width = 20, command=self.open_img_file_fcn)
+        open_img_file_btn.place(x=0, y=0)
+
+        open_audio_file_btn = tk.Button(tab1, text='Open audio file', height = 2, width = 20, command=self.open_audio_file_fcn)
+        open_audio_file_btn.place(x=0, y=55)
+
+        preview_btn = tk.Button(tab1, text='Preview', height = 2, width = 20, command=self.preview_fcn)
+        preview_btn.place(x=0, y=105)
+
+        encode_btn = tk.Button(tab1, text='Encode', height = 2, width = 20, command=self.encode_fcn)
+        encode_btn.place(x=0, y=155)
+
+
+
+    def open_img_file_fcn(self):
+        print("image file opened")
+
+        filetypes = (
+        ("Image files", ("*.jpg", "*.png")),
+        ("All files", "*.*")
+        )
+
+        base_img_filename = fd.askopenfilename(
+            title = "Open a File",
+            initialdir="/",
+            filetypes=filetypes
+        )
+
+        if base_img_filename == "":
+            return
+
+        base_img_filename = "".join(base_img_filename)
+
+        img = Image.open(base_img_filename)
+        img1 = ImageTk.PhotoImage(img)
+
+        open_img_filename.config(text=base_img_filename.split("/")[-1])
+
+        base_img_width = img1.width()
+        base_img_height = img1.height()
+
+        
+
+        if base_img_width > base_img_height:
+            base_w_scale = 1
+            base_h_scale = base_img_height / base_img_width
+        elif base_img_height > base_img_width:
+            base_h_scale = 1
+            base_w_scale = base_img_width / base_img_height
+        elif base_img_width == base_img_height:
+            base_w_scale = 1
+            base_h_scale = 1
+        
+        print(base_w_scale)
+        print(base_h_scale)
+
+        w = int(500 * base_w_scale)
+        h = int(500 * base_h_scale)
+        resize_img = img.resize((w, h))
+        img = ImageTk.PhotoImage(resize_img)
+        disp_inputimg.config(image=img)
+        disp_inputimg.image = img
+
+
+        baseImgdims.config(text = f'witdth: {base_img_width} height: {base_img_height}')
+        baseImgdims.place_configure(y=500*base_h_scale+120)
+        
+        inputimgFrame.config(width=(500*base_w_scale+8), height=(500*base_h_scale+8))
+        
+
+        if base_img_filename == 1 or audio_filename == 1:
+            preview_btn.config(state="disabled")
+            encode_btn.config(state="disabled")
+        elif base_img_filename != 1 and audio_filename == 1:
+            decode_button.config(state="normal")
+        else:
+            preview_btn.config(state="normal")
+            encode_btn.config(state="normal")
+            #decode_btn.config(state="normal")
+
+        
+        return base_img_filename
+
+    def open_audio_file_fcn(self):
+        filetypes = (
+            ("Audio files", ("*.mp3", "*.wav")),
+            ("All files", "*.*")
+        )
+
+        audio_filename = fd.askopenfilename(
+            title = "Open a File",
+            initialdir="/",
+            filetypes=filetypes
+        )
+
+        if audio_filename == "":
+            return
+
+        audio_filename = "".join(audio_filename)
+
+        if base_img_filename == 1 or audio_filename == 1:
+            preview.config(state="disabled")
+            encode_button.config(state="disabled")
+            decode_button.config(state="disabled")
+        else:
+            preview.config(state="normal")
+            encode_button.config(state="normal")
+            decode_button.config(state="normal")
+
+        if audio_filename != 1:
+            play_audio_button.config(state="normal")
+            encrypt_audio_button.config(state="normal")
+            decrypt_audio_button.config(state="normal")
+
+        open_audio_filename.config(text=audio_filename.split("/")[-1])
+        open_audio_filename_tab3.config(text=audio_filename.split("/")[-1])
+
+        audioclip = pydub.AudioSegment.from_mp3(audio_filename)
+        audioarray = np.array(audioclip.get_array_of_samples())
+        
+        
+
+        open_audio_length.config(text=f"{math.floor(len(audioarray)/(48000*60))}m:{(len(audioarray) % (48000*60))/48000}s")
+
+        plotaudio()
+
+        return audio_filename
+
+    def preview_fcn(self):
+        preview.config(state="disabled")
+
+        im = Image.open(base_img_filename)
+            
+        a = np.array(im)
+        a[0][0][0] = 0
+
+        #audioclip = pydub.AudioSegment.from_mp3(audiofilename)
+        #audioarray = np.array(audioclip.get_array_of_samples())
+        #audioarray = audioarray + (2**16)/2
+        #audioarray = np.rint((audioarray / 2**16) * 999)
+
+        progressbar_tab1["value"] = 0
+        pb_tab1_value_label['text'] = "                                            "
+        root.update_idletasks()
+        
+        for x in range(len(a)):
+            if x < 500:
+                #print(x)
+                for y in range(len(a[x])):
+                    if y < 500:
+                        for z in range(len(a[x][y])):
+                            
+                            if a[x][y][z] >= 250:
+                                a[x][y][z] = 250
+                                
+
+                            a[x][y][z] = round(a[x][y][z]/10) * 10
+                            
+                            if x*len(a[x])+y < len(audioarray):
+                                digit = get_digit(audioarray[x*len(a[x])+y], z)
+                                if a[x][y][z] == 250 and digit > 5:
+                                    a[x][y][z] = 240
+                                colourValue = int(a[x][y][z]) + int(digit)
+
+                                if colourValue >= 256:
+                                    colourValue = 255
+
+                                a[x][y][z] = colourValue
+
+                if len(a) < 500:
+                    progressbar_tab1["value"] += 1/len(a) * 100
+                    pb_tab1_value_label['text'] = update_progress_label()
+                    root.update_idletasks()
+                else:
+                    progressbar_tab1["value"] += 1/500 * 100
+                    pb_tab1_value_label['text'] = update_progress_label()
+                    root.update_idletasks()
+
+
+        
+
+        img_preview_out = Image.fromarray(a)
+        img_preview = ImageTk.PhotoImage(img_preview_out)
+
+        imgwidth = img_preview.width()
+        imgheight = img_preview.height()
+
+
+        if imgwidth >= 500 and imgheight >= 500:
+            img_preview_out = img_preview_out.crop((0,0,499,499))
+        elif imgwidth < 500 and imgheight >= 500:
+            img_preview_out = img_preview_out.crop((0,0,imgwidth-1,499))
+        elif imgwidth >= 500 and imgheight < 500:
+            img_preview_out = img_preview_out.crop((0,0,499,imgheight-1))
+        if imgwidth < 500 and imgheight < 500:
+            img_preview_out = img_preview_out.crop((0,0,imgwidth-1,imgheight-1))
+            
+
+        
+        img_preview_out = ImageTk.PhotoImage(img_preview_out)
+
+        if imgwidth < 500:
+            outputFrame.config(width=(imgwidth+8))
+        else:
+            outputFrame.config(width=508)
+        if imgheight < 500:
+            outputFrame.config(height=(imgheight+8))
+        else:
+            outputFrame.config(height=508)
+
+        disp_img2.config(image=img_preview_out)
+        disp_img2.image = img_preview_out
+
+        if imgheight > 500:
+            imgheight = 500
+
+        preview_img_label.place_configure(y=imgheight+120)
+        preview.config(state="normal")   
+
+    def encode_fcn(self):
+        f = fd.asksaveasfile(mode='w', defaultextension=".png")
+
+        im = Image.open(base_img_filename)
+        
+        a = np.array(im)
+
+        # popup progress bar code ######
+        global general_progress_bar1, progressbar_popup, general_pb_label, progressbar_label
+
+        progressbar_popup = tk.Toplevel(height=100, width=400)
+        progressbar_label = tk.Label(progressbar_popup, text="Audio is being encoded")
+        progressbar_label.place(x=150, y=25)
+        
+        general_progress_bar1 = ttk.Progressbar(progressbar_popup, orient="horizontal", length=350, mode='determinate')
+        general_progress_bar1.place(x=25, y=50)
+
+        general_pb_label = ttk.Label(progressbar_popup, text="0%")
+        general_pb_label.place(x=150,y=75)
+
+        progressbar_popup.update_idletasks()
+        
+        ###############################
+
+        #audioclip = pydub.AudioSegment.from_mp3(audiofilename)
+        #audioarray = np.array(audioclip.get_array_of_samples())
+        #audioarray = audioarray + (2**16)/2
+        #audioarray = np.rint((audioarray / 2**16) * 999)
+
+        
+        
+        if encrypt_check.get() == 1:
+            encrypt_audio()
+            audioarray_to_encode = audio_array_output
+            progressbar_label.config(text="Audio is being encoded")
+            progressbar_popup.update_idletasks()
+        elif encrypt_check.get() == 0:
+            audioarray_to_encode = audioarray
+
+
+        for x in range(len(a)):
+            #print(x)
+            for y in range(len(a[x])):
+                for z in range(len(a[x][y])):
+                    
+                    if a[x][y][z] >= 250:
+                        a[x][y][z] = 250
+                        
+
+                    a[x][y][z] = round(a[x][y][z]/10) * 10
+                    
+                    if x*len(a[x])+y < len(audioarray_to_encode):
+                        digit = get_digit(audioarray_to_encode[x*len(a[x])+y], z)
+                        if a[x][y][z] == 250 and digit > 5:
+                            a[x][y][z] = 240
+                        colourValue = int(a[x][y][z]) + int(digit)
+
+                        if colourValue >= 256:
+                            colourValue = 255
+
+                        a[x][y][z] = colourValue
+
+                    
+                    #print(str(digit) + "  " + str(audioarray[x*len(a[x])+y]))
+            
+            general_progress_bar(x, len(a))
+            progressbar_popup.update_idletasks()
+            
+        
+                    
+
+        #print(len(audioarray))
+
+    
+
+        im2 = Image.fromarray(a)
+        #im2 . show()
+        im2.save(str(f.name), format="png")   
+
+
+
+
+
+
+
+
+
 
 def general_progress_bar(increment, total):
     general_progress_bar1["value"] = increment/total * 100
@@ -65,7 +383,7 @@ def general_progress_bar(increment, total):
 def update_general_pb():
     return f"Current Progress: {round(general_progress_bar1['value'],1)}%"
 
-def select_image_file():
+""" def select_image_file():
     global base_img_filename
     global img
     global img1
@@ -138,7 +456,7 @@ def select_image_file():
         decode_button.config(state="normal")
 
     
-    return base_img_filename
+    return base_img_filename """
 
 def select_encoded_image_file():
     global encoded_img_filename
@@ -203,7 +521,7 @@ def select_encoded_image_file():
     
     return encoded_img_filename
 
-def select_audio_file():
+""" def select_audio_file():
     global audio_filename
     global audioarray
 
@@ -249,9 +567,9 @@ def select_audio_file():
 
     plotaudio()
 
-    return audio_filename
+    return audio_filename """
 
-def preview_encoded_image():
+""" def preview_encoded_image():
     preview.config(state="disabled")
 
     im = Image.open(base_img_filename)
@@ -340,9 +658,9 @@ def preview_encoded_image():
         imgheight = 500
 
     preview_img_label.place_configure(y=imgheight+120)
-    preview.config(state="normal")   
+    preview.config(state="normal")   """ 
 
-def encoding():
+""" def encoding():
     #root.update_idletasks()
     f = fd.asksaveasfile(mode='w', defaultextension=".png")
 
@@ -420,7 +738,7 @@ def encoding():
 
     im2 = Image.fromarray(a)
     #im2 . show()
-    im2.save(str(f.name), format="png")
+    im2.save(str(f.name), format="png") """
 
 def decoding():
     global base_img_filename
@@ -695,32 +1013,31 @@ def Exportaudio():
 
 
 
-
 # Progress bar window ###############################################
 
 
 
 
 # Tab 1 ##############################################################
-open_img_button = tk.Button(
+""" open_img_button = tk.Button(
     tab1,
     text='Open an image file',
     height = 2, 
     width=20,
     command=select_image_file
 )
-open_img_button.place(x=0, y=5)
+open_img_button.place(x=0, y=5) """
 
-open_audio_button = tk.Button(
+""" open_audio_button = tk.Button(
     tab1,
     text='Open an audio file',
     height = 2, 
     width=20,
     command=select_audio_file
 )
-open_audio_button.place(x=0, y=55)
+open_audio_button.place(x=0, y=55) """
 
-preview = tk.Button(
+""" preview = tk.Button(
     tab1,
     text='Preview',
     height = 2, 
@@ -736,7 +1053,7 @@ encode_button = tk.Button(
     width=20,
     command=encoding
 )
-encode_button.place(x=0, y=155)
+encode_button.place(x=0, y=155) """
 
 baseImgdims = tk.Label(
     tab1,
@@ -881,14 +1198,14 @@ textBox.place(x=0, y=405)
 
 
 # Tab 3 ##############################################################
-open_audio_button2 = tk.Button(
+""" open_audio_button2 = tk.Button(
     tab3,
     text='Open an audio file',
     height = 2, 
     width=20,
-    command=select_audio_file
+    command=open_encoded_img_button.self.open_audio_file_fcn
 )
-open_audio_button2.place(x=0, y=5)
+open_audio_button2.place(x=0, y=5) """
 
 open_audio_filename_tab3 = tk.Label(
     tab3,
@@ -995,4 +1312,5 @@ else:
     Exportoutputaudiobutton.config(state="disabled")
 
 # run the application
+testingclass1 = tab1_class(tab1)
 root.mainloop()
