278a4309517b78670721a0b336b622bc8899f9a5
Brian Neufeld <36906721+Brian-Neufeld@users.noreply.github.com>
2022-11-27 11:17:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import pybind11
 cpp_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7']
 
 sfc_module = Extension(
-    'superfastcodeloop',
-    sources=['superfastcodeloop.cpp'],
+    'encryptionmodule',
+    sources=['encryptionmodule.cpp'],
     include_dirs=[pybind11.get_include()],
     language='c++',
     extra_compile_args=cpp_args,
     )
 
 setup(
-    name='superfastcodeloop',
+    name='encryptionmodule',
     version='1.0',
-    description='Python package with C++ extension (PyBind11)',
+    description='Python package for data encryption',
     ext_modules=[sfc_module],
 )
